5a555bcb967a19035c41ab1e73d2045577cd1e99
MarotiMehatri-97 <mehatrimaroti@gmail.com>
2026-07-21 19:57:10 +0530

    backend folder structure create

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,10 +5,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
+    seed: "tsx prisma/seed.ts"
+  }
 });
